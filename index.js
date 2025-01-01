f2ab82f89f7ae324c912afb2697797e666e52bbb
Syed Muhammad Ahris <syed.ahris@sofstica.com>
2025-01-01 11:27:34 +0500

    changed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import * as core from "@actions/core";
 import { Octokit } from "@octokit/rest";
 import axios from "axios";
 import { readFileSync } from "fs";
+import minimatch from "minimatch";
 import parseDiff from "parse-diff";
 
 (async () => {
